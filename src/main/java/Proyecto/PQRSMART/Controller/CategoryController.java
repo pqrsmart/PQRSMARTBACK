8e593d52fc28f68a5d7737790c47d24b77d47b77
pqrsmart <pqrsmart@gmail.com>
2024-10-13 21:22:53 -0500

    Update CategoryController.java

--- src/main/java/Proyecto/PQRSMART/Controller/CategoryController.java
+++ src/main/java/Proyecto/PQRSMART/Controller/CategoryController.java
@@ -60,6 +60,7 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> activate(@PathVariable Long id){
         Optional<CategoryDTO> categoryDTOOptional = categoryService.findById(id);
         if(categoryDTOOptional.isPresent()) {
+            
             CategoryDTO categoryDTO = categoryDTOOptional.get();
             categoryDTO.setState(new State(1l, "ACTIVADO"));
             categoryService.activate(categoryDTO);
